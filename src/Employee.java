a3153631ab17d31a824076653bb069da6cb55d80
Harry Taylor <harrytaylor170503@gmail.com>
2025-10-13 17:06:01 +0100

    EDIT: changed employee class to include static retirement age

--- src/Employee.java
+++ src/Employee.java
@@ -1,22 +1,29 @@
 public class Employee {
+    private static int retirementAge = 65;
+    private String firstName;
     private String lastName;
     private int age;
 
-    // Constructor
+    // Constructors
     public Employee(String lastName, int age) {
         this.lastName = lastName;
         this.age = age;
     }
 
-    public void incAge() {
-        if (age < 65) age += 1;
+    public Employee(String firstName, String lastName, int age) {
+        this(lastName, age);
+        this.firstName = firstName;
     }
+
+
+    public void incAge() {if (age < retirementAge) age += 1;}
 
     // Setters
     public void setLastName(String lastName) {this.lastName = lastName;}
     public void setAge(int age) {this.age = age;}
+    public static void setRetirementAge(int age) {retirementAge = age;}
 
     // Getters
-    public String getLastName() {return lastName;}
+    public String getName() {return firstName + " " + lastName;}
     public int getAge() {return age;}
 }
--- src/EmployeeTest.java
+++ src/EmployeeTest.java
@@ -2,17 +2,20 @@ public class EmployeeTest {
 
     public static void main(String[] args) {
         Employee[] employees = new Employee[2];
-        employees[0] = new Employee("Taylor", 22);
-        employees[1] = new Employee("Punia", 25);
+        employees[0] = new Employee("Harry", "Taylor", 22);
+        employees[1] = new Employee("Jorovar", "Punia", 25);
 
         for (Employee employee : employees) {
-            System.out.println("Last name: " + employee.getLastName() + ", Age: " + employee.getAge());
+            System.out.println("Last name: " + employee.getName() + ", Age: " + employee.getAge());
         }
 
-        employees[0].incAge();
+        Employee.setRetirementAge(70);
+
+        int increments = 100;
+        for (int i = 0; i < increments; i++) employees[0].incAge();
 
         for (Employee employee : employees) {
-            System.out.println("Last name: " + employee.getLastName() + ", Age: " + employee.getAge());
+            System.out.println("Last name: " + employee.getName() + ", Age: " + employee.getAge());
         }
     }
 }
