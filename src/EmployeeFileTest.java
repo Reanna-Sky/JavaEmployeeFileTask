e773911d40d365e468c62f348a337bdc91484c3b
Reanna-Sky <reannagibsonsky@gmail.com>
2025-10-15 14:46:27 +0100

    Step 10 - created data.txt and showemployees method

--- src/EmployeeFileTest.java
+++ src/EmployeeFileTest.java
@@ -1,8 +1,12 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class EmployeeFileTest {
+
+    public static final String filePath = "/Users/rgo895/Documents/Java/exercises/student/JavaEmployeeFileTask/src/data.txt";
+
     public static void main(String[] args) {
         try {
             inputEmployee();
@@ -36,7 +40,19 @@ public class EmployeeFileTest {
 
     }
 //    Displayes content of file
-    public static void showEmployees() {
+    public static void showEmployees() throws IOException{
+        FileInputStream file = new FileInputStream(filePath);
+        InputStreamReader inputReader = new InputStreamReader(file);
+        BufferedReader buffer = new BufferedReader(inputReader);
+
+        String data;
+        do {
+            data = buffer.readLine();
+            System.out.println(data);
+        } while (data != null);
+
+        buffer.close();
+
 
 
     }
