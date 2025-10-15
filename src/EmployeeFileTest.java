2d845d77dbb2874eb91fbff6ffa8f3d2a7e96c82
Reanna-Sky <reannagibsonsky@gmail.com>
2025-10-15 14:14:57 +0100

    steps 1 - 7. Created getInput and Input employee. Created a try/catch with IOException of  problem with file handling)

---
+++ src/EmployeeFileTest.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EmployeeFileTest {
+    public static void main(String[] args) {
+        try {
+            inputEmployee();
+//            We are naming the exception as e
+        }catch (IOException e ){
+            System.out.println("Problem with file handling");
+        }
+    }
+
+    public static String getInput(String prompt) throws IOException {
+        System.out.println(prompt);
+
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(input);
+        return buffer.readLine();
+
+    }
+//    Asks for employees details
+    public static void inputEmployee() throws IOException {
+        String firstName = getInput("Enter the Employees first name: ");
+        String lastName = getInput("Enter the Employees last name: ");
+
+        String name = firstName + " " + lastName;
+
+        String strAge = getInput("Enter the Employees age: ");
+        int age = Integer.parseInt(strAge);
+
+
+    }
+//    Displayes content of file
+    public static void showEmployees() {
+
+
+    }
+}
